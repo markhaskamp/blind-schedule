f4f132a1ba8faf091a19876d63ff6d2a661d1afb
Mark Haskamp <mrhask@gmail.com>
2022-12-20 22:36:26 -0500

    highlight current stop

--- blind-schedule/src/components/BlindSchedule.vue
+++ blind-schedule/src/components/BlindSchedule.vue
@@ -1,44 +1,48 @@
 <script>
 import { ref } from 'vue'
 
-let foo
+let intervalID
 
 export default {
   mounted() {
-    // console.log('mounted(). ingress')
     this.startSeconds = Math.floor(Date.now() / 1000)
-
-    foo = setInterval(this.someFunc, 250)
+    intervalID = setInterval(this.handleIntervalTick, 250)
   },
+
   data() {
     return {
       startSeconds: 0,
       currentStep: 0,
       countDownValue: "",
       isRunning: true,
-      startTime: "",
       blindSchedule: [
-        { step: 1, minutes: 30, smallBlind: 50, bigBlind: 100 },
-        { step: 2, minutes: 30, smallBlind: 100, bigBlind: 200 },
-        { step: 3, minutes: 30, smallBlind: 150, bigBlind: 300 },
-        { step: 4, minutes: 15, smallBlind: 200, bigBlind: 400 }
+        { step: 1, minutes: 30, smallBlind: 50, bigBlind: 100},
+        { step: 2, minutes: 30, smallBlind: 100, bigBlind: 200},
+        { step: 3, minutes: 30, smallBlind: 150, bigBlind: 300},
+        { step: 4, minutes: 15, smallBlind: 200, bigBlind: 400}
       ]
     }
   },
+
   methods: {
-    someFunc() {
+    handleIntervalTick() {
         if (this.isRunning) {
-          let currentSeconds = Math.floor(Date.now() / 1000)
-          let elapsedSeconds = currentSeconds - this.startSeconds
-          let x = ((this.blindSchedule[this.currentStep]['minutes'] * 60) - elapsedSeconds)
-          let minutes = Math.floor(x / 60)
-          let seconds = Math.floor(x % 60)
-          if (seconds < 10) {
-            this.countDownValue = `${minutes}:0${seconds}`
-          } else {
-            this.countDownValue = `${minutes}:${seconds}`
-          }
+          let display = this.getDisplayTimer()
+          this.countDownValue = `${display.minutes}:${display.seconds}`
         }
+    },
+
+    getDisplayTimer() {
+      let currentSeconds = Math.floor(Date.now() / 1000)
+      let elapsedSeconds = currentSeconds - this.startSeconds
+      let x = ((this.blindSchedule[this.currentStep]['minutes'] * 60) - elapsedSeconds)
+      let minutes = Math.floor(x / 60)
+      let seconds = Math.floor(x % 60)
+      if (seconds < 10) {
+        return {"minutes": minutes, "seconds": `0${seconds}`}
+      } else {
+        return {"minutes": minutes, "seconds": seconds}
+      }
     }
   }
 }
@@ -48,10 +52,18 @@ export default {
 <template>
   <h1>{{ countDownValue }}</h1>
   <div v-for="s in this.blindSchedule" :key="s.step">
-    <span>{{s.step}}</span> - <span>{{s.minutes}}</span> - <span>{{s.smallBlind}}</span><span>{{s.bigBlind}}</span>
+    <div :class="{ currentstep: (s.step == this.currentStep+1) }">
+      <span>{{s.step}}</span> - 
+      <span>{{s.minutes}}</span> - 
+      <span>{{s.smallBlind}}</span> - 
+      <span>{{s.bigBlind}}</span>
+    </div>
   </div>
 
 </template>
 
 <style scoped>
+.currentstep {
+  border: 1px solid red;
+}
 </style>
