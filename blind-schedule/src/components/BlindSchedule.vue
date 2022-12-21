2618324eb6eab31f5b822133000d5c394bd87e70
Mark Haskamp <mrhask@gmail.com>
2022-12-21 16:13:02 -0500

    pause / resume

--- blind-schedule/src/components/BlindSchedule.vue
+++ blind-schedule/src/components/BlindSchedule.vue
@@ -12,6 +12,7 @@ export default {
   data() {
     return {
       startSeconds: 0,
+      bankedElapsedSeconds: [],
       currentStep: 0,
       countDownValue: "",
       isRunning: true,
@@ -19,7 +20,8 @@ export default {
         { step: 1, minutes: 0.25, smallBlind: 50, bigBlind: 100},
         { step: 2, minutes: 0.25, smallBlind: 100, bigBlind: 200},
         { step: 3, minutes: 0.5, smallBlind: 150, bigBlind: 300},
-        { step: 4, minutes: 10, smallBlind: 200, bigBlind: 400}
+        { step: 4, minutes: 15, smallBlind: 200, bigBlind: 400},
+        { step: 5, minutes: 15, smallBlind: 250, bigBlind: 500}
       ]
     }
   },
@@ -35,10 +37,18 @@ export default {
     getDisplayTimer() {
       let currentSeconds = Math.floor(Date.now() / 1000)
       let elapsedSeconds = currentSeconds - this.startSeconds
+      let bankedSeconds = this.bankedElapsedSeconds.reduce((acc, cur) => acc + cur, 0)
+      elapsedSeconds += bankedSeconds
+
       if (elapsedSeconds > (this.blindSchedule[this.currentStep]['minutes'] * 60)) {
         this.startSeconds = Math.floor(Date.now() / 1000)
+        this.bankedElapsedSeconds = []
         this.currentStep += 1
-        return {"minutes": this.blindSchedule[this.currentStep]['minutes'], "seconds": "00"}
+        let minutes = this.blindSchedule[this.currentStep]['minutes']
+        if (minutes < 1) {
+          minutes = 0
+        }
+        return {"minutes": minutes , "seconds": "00"}
       }
       let x = ((this.blindSchedule[this.currentStep]['minutes'] * 60) - elapsedSeconds)
       let minutes = Math.floor(x / 60)
@@ -48,7 +58,19 @@ export default {
       } else {
         return {"minutes": minutes, "seconds": seconds}
       }
+    },
+
+    handlePauseClick() {
+      if (this.isRunning) {
+        let currentSeconds = Math.floor(Date.now() / 1000)
+        let elapsedSeconds = currentSeconds - this.startSeconds
+        this.bankedElapsedSeconds.push(elapsedSeconds)
+      } else {
+        this.startSeconds = Math.floor(Date.now() / 1000)
+      }
+      this.isRunning = !this.isRunning
     }
+
   }
 }
 
@@ -57,7 +79,7 @@ export default {
 <template>
   <div class="timer">{{ countDownValue }}</div> 
   <div class="timerControls">
-    <input type="button" value="Pause"/>
+    <button id="pauseButton" @click="handlePauseClick">{{ this.isRunning ? 'Pause' : 'Resume' }}</button>
   </div>
   <div v-for="s in this.blindSchedule" :key="s.step">
     <div :class="{ currentstep: (s.step == this.currentStep+1) }">
@@ -72,10 +94,16 @@ export default {
 
 <style scoped>
 .currentstep {
-  border: 1px solid red;
+  color: #006600;
+  background-color: white;
+  font-weight: bold;
 }
 .timerControls {
   padding-top: 0.5em;
   padding-bottom: 1.5em;
+}
+#pauseButton {
+  width: 100px;
+  height: 2.25em;
 }
 </style>
